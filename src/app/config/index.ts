40dbb7fe92fb9f66e969707562c8555fb1565377
Nimul99 <htnimul99@gmail.com>
2024-06-21 22:51:27 +0600

    .

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   port: process.env.PORT || 5000,
   database_url: process.env.DATABASE_URL,
-  jwt_secret: process.env.JWT_SECRET || "your_jwt_secret",
+  jwt_secret: process.env.JWT_SECRET,
   bcrypt_salt_rounds: process.env.BCRYPT_SALY_ROUNDS,
   default_password: process.env.DEFAULT_PASS,
 };
